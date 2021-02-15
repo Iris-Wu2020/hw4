e1157f173fda2f51dfdd6db5419ed9b733c82f98
IrisWu <xianwu_iris@126.com>
2021-02-15 08:28:43 +0800

    first commit

---
+++ js/ex1.js
@@ -0,0 +1,7 @@
+/* Tutorial 4
+Example 1 JavaScript code
+*/
+console.log("Wu's Output from hw4 Exercise 1");
+
+let number1 = Number(prompt("Please enter an integer:"));
+let number2 = Number(prompt("Please enter another integer:"));
