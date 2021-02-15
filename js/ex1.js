67c0e331f1b4aa65c766d5c1ade799c0a868b83a
IrisWu <xianwu_iris@126.com>
2021-02-15 17:54:50 +0800

    change

--- js/ex1.js
+++ js/ex1.js
@@ -3,5 +3,30 @@ Example 1 JavaScript code
 */
 console.log("Wu's Output from hw4 Exercise 1");
 
+const calculationFunction = (number1, number2, operation) => {
+    switch (operation) {
+    case "+":
+        console.log(`${number1} + ${number2} = ${number1 + number2}`);
+        break;
+    case "-":
+        console.log(`${number1} - ${number2} = ${number1 - number2}`);
+        break;
+    case "*":
+        console.log(`${number1} * ${number2} = ${number1 * number2}`);
+        break;
+    case "/":
+        console.log(`${number1} / ${number2} = ${number1 / number2}`);
+        break;
+    default:
+        console.log("The operation you entered is invalid!");
+    }
+
+}
+
 let number1 = Number(prompt("Please enter an integer:"));
 let number2 = Number(prompt("Please enter another integer:"));
+let operation = prompt("Please enter a mathematical operation: +, =, *, or /");
+calculationFunction(`${number1}`,`${number2}`, `${operation}`)
+
+
+
