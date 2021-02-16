8a1d2340477f17da1205dea3d40eafd793c09864
IrisWu <xianwu_iris@126.com>
2021-02-16 09:13:36 +0800

    change

--- js/ex4.js
+++ js/ex4.js
@@ -1,1 +1,21 @@
 console.log("Wu's Output from hw4 Exercise 4");
+
+let word = prompt("Please enter a word:");
+if (`${word.toLowerCase()}` === "stop") {
+    console.log("Bingo!");
+} 
+else {
+    let wordArray = []
+    let count = 0
+    while (`${word.toLowerCase()}`!== "stop") {
+        wordArray.push(`${word}`);
+        count++;
+        word = prompt("Please enter another word:");
+    }
+
+    console.log("You entered the following words:");
+    for (i = 0; i < wordArray.length; i++) {
+        console.log(wordArray[i]);
+    }
+}
+
