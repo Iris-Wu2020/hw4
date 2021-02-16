b2bcd95acd052e404f101a1e5f053df2b83d1553
IrisWu <xianwu_iris@126.com>
2021-02-16 17:59:27 +0800

    change

--- js/ex5.js
+++ js/ex5.js
@@ -1,8 +1,22 @@
 console.log("Wu's Output from hw4 Exercise 5");
 
 let word = prompt("Please enter a word:");
-let left;
-let right;
-left = 0;;
-right = word.length - 1;
-console.log(right)
+
+const isPalindrome = (s) => {
+    let left;
+    let right;
+    left = 0;;
+    right = s.length - 1;
+    while (left < right) {
+        if ((left < right) && (s.toLowerCase()[left] !== s.toLowerCase()[right])) {
+            return false
+        }
+        right++;
+        left--;
+    }
+    return true
+
+}
+
+
+
--- js/ex6.js
+++ js/ex6.js
@@ -1,1 +1,16 @@
 console.log("Wu's Output from hw4 Exercise 6");
+
+let randomNumber = Math.trunc(Math.random() * 100 + 1);
+let number = Number(prompt("Enter an integer:"));
+let count = 0;
+while (randomNumber !== number) {
+    if (number < randomNumber) {
+        number = Number(prompt("Too low, guess again!"));
+        count++;
+    }
+    if (number > randomNumber) {
+        number = Number(prompt("Too high, guess again!"));
+        count++;
+    }
+}
+console.log(`Correct! It took you ${count} attempts to guess the correct number.`)
